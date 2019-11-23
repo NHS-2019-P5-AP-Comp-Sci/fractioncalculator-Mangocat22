8eeceef9bb41806299a0d9e19faa0953d9f7c911
s-richardsona <s-richardsona@53S50031137.it.bsd405.org>
2019-11-22 22:35:36 -0800

    This is my code for checkpoint 2 fracCalc.

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -40,9 +40,119 @@ public class FracCalc {
 	for (int i=0;i<pointofoperator;i++) {
 		firstpacket=firstpacket+input.charAt(i);
 	}	
+	
 	for (int i = 0; i < input.length() - 3 - pointofoperator; i++) {
 		secondpacket = secondpacket + input.charAt(3 + pointofoperator + i);
 		}
-			return secondpacket;
-		}
+
+	
+	int firstpacketWhole=Integer.parseInt(whole(firstpacket));
+	int secondpacketWhole=Integer.parseInt(whole(secondpacket));
+	int firstpacketNumberator=Integer.parseInt(numerator(firstpacket));
+	int secondpacketNumerator=Integer.parseInt(numerator(secondpacket));
+	int firstpacketDenominator=Integer.parseInt(denominator(firstpacket));
+	int secondpacketDenominator=Integer.parseInt(denominator(secondpacket));
+	
+return("whole:"+secondpacketWhole+" " +"numerator:"+secondpacketNumerator+" "+"denominator:"+secondpacketDenominator);
+	}
+	public static String whole(String fraction) {
+		String wholenum="";
+ if (fraction.indexOf("/") == -1) {
+
+wholenum = fraction;
+
+} else if (fraction.indexOf("_") != -1) {
+
+int numbersBeforeUnderscore = fraction.indexOf("_");
+
+for (int i = 0; i < numbersBeforeUnderscore; i++) {
+
+wholenum = wholenum + fraction.charAt(i);
+
+}
+} else if (fraction.indexOf("_") == -1) {
+
+wholenum = "0";
+
+}
+
+return wholenum;
+
+}
+
+
+public static String numerator(String fraction) {
+
+String numeratornum = "";
+
+if (fraction.indexOf("/") == -1) {
+
+numeratornum = "0";
+
+} else if (fraction.indexOf("_") != -1) {
+
+int numsBeforeSlash = fraction.indexOf("/");
+
+int numsBeforeUnderscore = fraction.indexOf("_");
+
+for (int i = 0; i < numsBeforeSlash - numsBeforeUnderscore - 1; i++) {
+
+numeratornum = numeratornum + fraction.charAt(numsBeforeUnderscore + 1 + i);
+
+}
+
+} else if (fraction.indexOf("_") == -1) {
+
+int numsBeforeSlash = fraction.indexOf("/");
+
+for (int i = 0; i < numsBeforeSlash; i++) {
+
+numeratornum = numeratornum + fraction.charAt(i);
+
+}
+
+}
+
+return numeratornum;
+
+}
+
+//finds the denominator of a fraction
+
+public static String denominator(String fraction) {
+
+String denominatornum = "";
+
+if (fraction.indexOf("/") == -1) {
+
+denominatornum = "1";
+
+} else if (fraction.indexOf("_") != -1) {
+
+int numsBeforeSlash = fraction.indexOf("/");
+
+for (int i = 0; i < fraction.length() - numsBeforeSlash - 1; i++) {
+
+denominatornum = denominatornum + fraction.charAt(numsBeforeSlash + 1 + i);
+
+}
+
+} else if (fraction.indexOf("_") == -1) {
+
+int numsBeforeSlash = fraction.indexOf("/");
+
+for (int i = 0; i < fraction.length() - numsBeforeSlash - 1; i++) {
+
+denominatornum = denominatornum + fraction.charAt(numsBeforeSlash + 1 + i);
+
+}
+
+}
+
+return denominatornum;
+
+}
+
+
+
 }
