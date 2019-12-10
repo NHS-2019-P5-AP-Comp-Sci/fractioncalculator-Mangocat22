38bb1379404d31a9c5a187adccedc7a393da2b32
Alex Richardson <s-richardsona@53S50031137.it.bsd405.org>
2019-12-09 18:15:22 -0800

    This is the final version of my FracCalc project.

--- src/main/java/fracCalc/FracCalc.java
+++ src/main/java/fracCalc/FracCalc.java
@@ -3,174 +3,180 @@ package fracCalc;
 import java.util.Scanner;
 
 public class FracCalc {
+	// Defines calculatedNumeratorValue and calculatedDenominatorValue as static
+	// integer values so that their scope is the entire class FracCalc.
+	// These are updated throughout the program to mitigate redundancy (instead of
+	// defining new data points)
+	static int calculatedNumeratorValue;
+	static int calculatedDenominatorValue;
 
 	public static void main(String[] args) {
-		Scanner Userinput = new Scanner(System.in);
-		String input = Userinput.nextLine();
-		while (!(input.toUpperCase()).equals("QUIT")) {
+		System.out.println("To exit FracCalc, enter \"quit\".");
+		Scanner feedback = new Scanner(System.in);
+		String input = feedback.nextLine(); // The fractions the user writes make up input
+		while (!input.equalsIgnoreCase("quit")) { // Run the function unless the user writes quit (capitalized or not)
 			System.out.println(produceAnswer(input));
-			input = Userinput.nextLine();
-			
+			System.out.println("To exit FracCalc, enter \"quit\".");
+			input = feedback.nextLine();
 		}
+		feedback.close();
+		System.out.println("Here are the results.");
 	}
 
+	// ** IMPORTANT ** DO NOT DELETE THIS FUNCTION. This function will be used to
+	// test your code
+	// This function takes a String 'input' and produces the result
+	//
+	// input is a fraction string that needs to be evaluated. For your program, this
+	// will be the user input.
+	// e.g. input ==> "1/2 + 3/4"
+	//
+	// The function should return the result of the fraction after it has been
+	// calculated
+	// e.g. return ==> "1_1/4"
 	public static String produceAnswer(String input) {
-		int pointofoperator = 0;
-		String operation = "";
-		int addition = input.indexOf(" + ");
-		int subtraction = input.indexOf(" - ");
-		int division = input.indexOf(" / ");
-		int multiplication = input.indexOf(" + ");
-
-		if (addition != -1) {
-			pointofoperator = addition;
-			operation = "+";
-		} else if (subtraction != -1) {
-			pointofoperator = subtraction;
-			operation = "-";
-		} else if (division != -1) {
-			pointofoperator = division;
-			operation = "/";
-		} else if (multiplication != -1) {
-			pointofoperator = multiplication;
-			operation = "*";
+// Finds the space and gives it a positional value
+		int positionofspace = input.indexOf(" ");
+		if (positionofspace == -1) {
+			return ("ERROR");
+		} else {
+// Pulls apart user input by parsing and stores the various parts of each fraction (like numerators, denominators, and operations).
+			String datapiece = pullData(input);
+			input = updateRawInput(input);
+			parseFraction(datapiece);
+			int firstPacketNumerator = calculatedNumeratorValue;
+			int firstPacketDenominator = calculatedDenominatorValue;
+// First round (above) for first fraction
+			datapiece = pullData(input);
+			input = updateRawInput(input);
+			String operation = datapiece;
+// Second round (above) for operation
+			datapiece = pullData(input);
+			input = updateRawInput(input);
+			parseFraction(datapiece);
+			int secondPacketNumerator = calculatedNumeratorValue;
+			int secondPacketDenominator = calculatedDenominatorValue;
+// Third round (above) for second fraction
+			boolean calcCheck;
+// calcCheck is a boolean that is true if the user input follows proper formatting and mathematical rules.
+			calcCheck = calculator(firstPacketNumerator, firstPacketDenominator, secondPacketNumerator,
+					secondPacketDenominator, operation);
+			if (calcCheck) {
+				return print(calculatedNumeratorValue, calculatedDenominatorValue);
+			} else {
+				return "ERROR";
+			}
 		}
+	}
 
-		String firstpacket = "";
-		String secondpacket = "";
-		for (int i = 0; i < pointofoperator; i++) {
-			firstpacket = firstpacket + input.charAt(i);
+// Static method used for parsing in the method produceAnswer. It isolates a section of a string to create a substring that is then parsed.
+	public static String pullData(String input) {
+		int positionofspace = input.indexOf(' ');
+		if (positionofspace == -1)
+			return input;
+		return input.substring(0, positionofspace);
+	}
+
+// Static method that takes the input returned from the method pullData and updates for future use.   
+// The paired calling of pullData and updateRawInput parses the user feedback packets into useful components.
+	public static String updateRawInput(String input) {
+		int space = input.indexOf(" ");
+		if (space == -1)
+			return "";
+		return input.substring(space + 1);
+	}
+
+	public static void parseFraction(String input) {
+
+		int underscore = input.indexOf('_');
+		int frontslash = input.indexOf('/');
+		if (underscore > 0) {
+// This part of the if statement only runs if the user inputs a mixed number
+			int whole = Integer.parseInt(input.substring(0, underscore));
+			calculatedNumeratorValue = Integer.parseInt(input.substring(underscore + 1, frontslash));
+			if (whole < 0)
+// This part of the if statement only runs if the whole number part of the mixed fraction is negative
+				calculatedNumeratorValue = 0 - calculatedNumeratorValue;
+			calculatedDenominatorValue = Integer.parseInt(input.substring(frontslash + 1));
+			calculatedNumeratorValue = calculatedNumeratorValue + whole * calculatedDenominatorValue;
+		} else if (frontslash > 0) {
+// This part of the if statement only runs if the user inputs a mixed number. It logically is paired with the first if statement.
+			calculatedNumeratorValue = Integer.parseInt(input.substring(0, frontslash));
+			calculatedDenominatorValue = Integer.parseInt(input.substring(frontslash + 1));
+		} else {
+// This part of the if statement only runs if the user inputs a whole number.
+			calculatedNumeratorValue = Integer.parseInt(input);
+			calculatedDenominatorValue = 1;
 		}
+	}
 
-		for (int i = 0; i < input.length() - 3 - pointofoperator; i++) {
-			secondpacket = secondpacket + input.charAt(3 + pointofoperator + i);
-		}
-
-		int firstpacketWhole = Integer.parseInt(whole(firstpacket));
-		int secondpacketWhole = Integer.parseInt(whole(secondpacket));
-		int firstpacketNumerator = Integer.parseInt(numerator(firstpacket));
-		int secondpacketNumerator = Integer.parseInt(numerator(secondpacket));
-		int firstpacketDenominator = Integer.parseInt(denominator(firstpacket));
-		int secondpacketDenominator = Integer.parseInt(denominator(secondpacket));
-
-		int firstFractionnumerator = ((firstpacketWhole * firstpacketDenominator) + firstpacketNumerator);
-
-		int secondFractionnumerator = ((secondpacketWhole * secondpacketDenominator) + secondpacketNumerator);
-		
-		int calculatedfirstfractionnumerator = firstFractionnumerator * secondpacketDenominator;
-		int calculatedfirstfractiondenominator = firstpacketDenominator * secondpacketDenominator;
-		int calculatedsecondfractionnumerator = secondFractionnumerator * firstpacketDenominator;
-		int calculatedsecondfractiondenominator = firstpacketDenominator * secondpacketDenominator;
-		String answer = "";
+	// this method runs the calculation and returns true if the user input follows
+	// all the rules. If not, it returns an error statement.
+	public static boolean calculator(int firstnumerator, int firstdenominator, int secondnumerator,
+			int seconddenominator, String operation) {
 		if (operation.equals("+")) {
-			answer = (calculatedfirstfractionnumerator + calculatedsecondfractionnumerator) + "/"
-					+ (calculatedfirstfractiondenominator);
+			calculatedNumeratorValue = firstnumerator * seconddenominator + secondnumerator * firstdenominator;
+			calculatedDenominatorValue = firstdenominator * seconddenominator;
 		} else if (operation.equals("-")) {
-			answer = (calculatedfirstfractionnumerator - calculatedsecondfractionnumerator) + "/"
-					+ (calculatedfirstfractiondenominator);
+			calculatedNumeratorValue = firstnumerator * seconddenominator - secondnumerator * firstdenominator;
+			calculatedDenominatorValue = firstdenominator * seconddenominator;
 		} else if (operation.equals("*")) {
-			answer = (firstpacketNumerator*secondpacketNumerator) + "/"
-					+ (firstpacketDenominator * secondpacketDenominator);
+			calculatedNumeratorValue = firstnumerator * secondnumerator;
+			calculatedDenominatorValue = firstdenominator * seconddenominator;
 		} else if (operation.equals("/")) {
-			answer = (calculatedfirstfractionnumerator * calculatedsecondfractiondenominator) + "/"
-					+ (calculatedfirstfractiondenominator * calculatedsecondfractionnumerator);
+			calculatedNumeratorValue = firstnumerator * seconddenominator;
+			calculatedDenominatorValue = firstdenominator * secondnumerator;
 		}
-		return answer;
-}
-		
-	public static String whole(String fraction) {
-		String wholenum = "";
-		if (fraction.indexOf("/") == -1) {
-
-			wholenum = fraction;
-
-		} else if (fraction.indexOf("_") != -1) {
-
-			int numbersBeforeUnderscore = fraction.indexOf("_");
-
-			for (int i = 0; i < numbersBeforeUnderscore; i++) {
-
-				wholenum = wholenum + fraction.charAt(i);
-
-			}
-		} else if (fraction.indexOf("_") == -1) {
-
-			wholenum = "0";
-
+		// Checks for user input formatting or mathematical rule error
+		else {
+			System.out.println("ERROR");
+			return false;
 		}
-
-		return wholenum;
-
+		return true;
 	}
 
-	public static String numerator(String fraction) {
-
-		String numeratornum = "";
-
-		if (fraction.indexOf("/") == -1) {
-
-			numeratornum = "0";
-
-		} else if (fraction.indexOf("_") != -1) {
-
-			int numsBeforeSlash = fraction.indexOf("/");
-
-			int numsBeforeUnderscore = fraction.indexOf("_");
-
-			for (int i = 0; i < numsBeforeSlash - numsBeforeUnderscore - 1; i++) {
-
-				numeratornum = numeratornum + fraction.charAt(numsBeforeUnderscore + 1 + i);
-
+	// calculates denominator
+	public static int calculateReducedDenominator(int numerator, int denominator) {
+		while (true) {
+			if (numerator < denominator) {
+				int a = numerator;
+				numerator = denominator;
+				denominator = a;
 			}
-
-		} else if (fraction.indexOf("_") == -1) {
-
-			int numsBeforeSlash = fraction.indexOf("/");
-
-			for (int i = 0; i < numsBeforeSlash; i++) {
-
-				numeratornum = numeratornum + fraction.charAt(i);
-
-			}
-
+			if (denominator == 0)
+				return numerator;
+			numerator = numerator % denominator;
 		}
-
-		return numeratornum;
-
 	}
 
-//finds the denominator of a fraction
-
-	public static String denominator(String fraction) {
-
-		String denominatornum = "";
-
-		if (fraction.indexOf("/") == -1) {
-
-			denominatornum = "1";
-
-		} else if (fraction.indexOf("_") != -1) {
-
-			int numsBeforeSlash = fraction.indexOf("/");
-
-			for (int i = 0; i < fraction.length() - numsBeforeSlash - 1; i++) {
-
-				denominatornum = denominatornum + fraction.charAt(numsBeforeSlash + 1 + i);
-
-			}
-
-		} else if (fraction.indexOf("_") == -1) {
-
-			int numsBeforeSlash = fraction.indexOf("/");
-
-			for (int i = 0; i < fraction.length() - numsBeforeSlash - 1; i++) {
-
-				denominatornum = denominatornum + fraction.charAt(numsBeforeSlash + 1 + i);
-
-			}
-
+// Prints the reduced version of the calculated fraction produced by the method calculator
+	public static String print(int finalnumerator, int finaldenominator) {
+		String result = "";
+// Switches negative on denominator to negative on numerator
+		if (finaldenominator < 0) {
+			finalnumerator = 0 - finalnumerator;
+			finaldenominator = Math.abs(finaldenominator);
 		}
-
-		return denominatornum;
+		if (finalnumerator < 0) {
+// Adds a negative to final numerator if it is negative
+			result += "-";
+			finalnumerator = Math.abs(finalnumerator);
+		}
+// Calculates greatest common denominator 
+		int commondem = calculateReducedDenominator(finalnumerator, finaldenominator);
+		finalnumerator = finalnumerator / commondem;
+		finaldenominator = finaldenominator / commondem;
+// Simplify
+		int wholenumber = finalnumerator / finaldenominator;
+		finalnumerator = finalnumerator % finaldenominator;
+		if (finalnumerator == 0) {
+			result += wholenumber;
+		} else {
+			if (wholenumber > 0) {
+				result += (wholenumber + "_");
+			}
+			result += (finalnumerator + "/" + finaldenominator);
+		}
+		return result;
 	}
 }
